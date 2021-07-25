e73cc34f70b26e7b4a8319352ec5f2dae80ad15b
Albert <albertbarsegyan6@gmail.com>
2021-07-25 23:23:22 +0400

    feat(tailwind.config.js): change config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
+    './helpers/**/*.{js,ts,jsx,tsx}',
+    // Add more here
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
