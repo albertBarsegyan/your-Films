2f3f0ac8cee0c83dda5b0f2203bf9a35d97d3ce7
Albert <70009994+albertBarsegyan@users.noreply.github.com>
2021-07-31 13:32:19 +0400

    Delete fetchMovieList.js

--- fetchMovieList.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-const API_KEY = 'f36f23edf6e10fd2ddcf939916b1f67a';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const GENRE_LIST = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`;
-axios.get(GENRE_LIST).then((res) => {
-  console.log(res.data);
-});
