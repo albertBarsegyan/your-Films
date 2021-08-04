2e30f5097eb0e05ab991e8dca2963efb476cc4ca
Albert <albertbarsegyan6@gmail.com>
2021-08-05 00:44:48 +0400

    feat(favorites.jsx): add exceptions for opening pagege
    if there wasn't favoriteList in local storage it throws an error message

--- src/pages/userAccount/favorites.jsx
+++ src/pages/userAccount/favorites.jsx
@@ -7,16 +7,15 @@ import SearchContainer from '../../components/search/SearchContainer';
 import MenuBlock from '../../components/userMenu/MenuBlock';
 
 export default function Favorites() {
-  let localFavoriteList;
+  let localFavoriteObject;
   let localUser;
   if (process.browser) {
     localUser = JSON.parse(localStorage.getItem('loggedUser'));
-    localFavoriteList =
-      (localStorage.getItem('favoriteList') &&
-        JSON.parse(localStorage.getItem('favoriteList')).find(
+    localFavoriteObject = localStorage.getItem('favoriteList')
+      ? JSON.parse(localStorage.getItem('favoriteList')).find(
           (favoriteObject) => favoriteObject.email === localUser.email
-        )) ||
-      {};
+        )
+      : undefined;
   }
   return (
     <div className="relative">
@@ -32,12 +31,19 @@ export default function Favorites() {
         </h3>
       </div>
       <div className="flex flex-col md:flex-row flex-wrap gap-5 mx-5 justify-center my-5">
-        {localFavoriteList &&
-          localFavoriteList.favoriteList.map((favorite) => {
+        {localFavoriteObject && localFavoriteObject.favoriteList.length > 0 ? (
+          localFavoriteObject.favoriteList.map((favorite) => {
             return (
               <FilmBlock makeFavorite key={favorite.id} filmObject={favorite} />
             );
-          })}
+          })
+        ) : (
+          <div className="w-full">
+            <p className="text-primary text-center text-4xl">
+              There isn&rsquo;t any favorite Movie
+            </p>
+          </div>
+        )}
       </div>
 
       <Footer />
